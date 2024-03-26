aca695c48f49934278a27828517740e6fea21756
ip-repo <123945379+ip-repo@users.noreply.github.com>
2024-03-26 21:30:15 +0200

    Create ConsoleExample.java

---
+++ java-examples/ConsoleExample.java
@@ -0,0 +1,18 @@
+import java.io.Console;
+
+/**
+ * This class is an example of using Console to collect a passowrd from user.
+ * This example also show how to use ansi to print a colored output.
+ */
+public class ConsoleExample {
+    //ansi color constants
+    public static final String ANSI_RESET = "\u001B[0m"; //add at the end of a colored string to reset.
+    public static final String ANSI_YELLOW = "\u001B[34m";
+    public static final String ANSI_RED = "\u001B[31m";
+        public static void main (String[] args) {
+        //create a console and ask the user a passowrd
+        Console console = System.console();
+        char[] password = console.readPassword(ANSI_YELLOW + "Enter password: " + ANSI_RESET);
+        console.printf(ANSI_RED + "You entered: %s%n", String.valueOf(password) +   ANSI_RESET);
+        }
+}
