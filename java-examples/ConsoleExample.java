966198ce78202198197139c784c5addd62ddd029
ip-repo <123945379+ip-repo@users.noreply.github.com>
2024-03-27 05:43:48 +0200

    Update ConsoleExample.java

--- java-examples/ConsoleExample.java
+++ java-examples/ConsoleExample.java
@@ -10,7 +10,7 @@ public class ConsoleExample {
     public static final String ANSI_YELLOW = "\u001B[34m";
     public static final String ANSI_RED = "\u001B[31m";
         public static void main (String[] args) {
-        //create a console and ask the user a passowrd
+        //create a console and ask the user a password
         Console console = System.console();
         char[] password = console.readPassword(ANSI_YELLOW + "Enter password: " + ANSI_RESET);
         console.printf(ANSI_RED + "You entered: %s%n", String.valueOf(password) +   ANSI_RESET);
